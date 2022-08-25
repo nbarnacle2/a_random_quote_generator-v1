a29e4c36ae793569a4303531c746b8319d851df7
nbarnacle2 <112004825+nbarnacle2@users.noreply.github.com>
2022-08-25 08:13:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-const quote1 ={
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//array with quotes
+var quotes = new Array(quote1 ={
   quote: 'Do you think love can bloom, even on a battlefield?',
   source: 'Otacon',
   citation: 'Metal Gear Solid',
   year: 1998
-};
-const quote2={
+},
+quote2={
   quote: 'It’s easy to forget what a sin is in the middle of a battlefield.',
   source: 'Metal Gear Solid',
   year: 1998
-};
-const quote3={
+},
+quote3={
   quote: '“I’ve never fought for anyone but myself.”',
   citation: 'Metal Gear Solid',
   source: 'Solid Snake',
   year: 1998
-};
-const quote4={
+},
+quote4={
   quote: 'Nanomachines, son! They harden in response to physical trauma. You cant hurt me, Jack!',
   source: 'Metal Gear Rising',
   year: 2013
-};
-const quote5={
+},
+quote5={
   quote: 'Everyone withdraws into their own small gated community, afraid of a larger forum. They stay inside their little ponds, leaking whatever "truth" suits them.',
   source: 'Metal Gear Solid 2',
   year: 2001
-};
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
-var quotes = new Array(quote1, quote2, quote3, quote4, quote5);
+});
 
+//function chooses a random quote from the array
 function getRandomQuote(){
 var random = Math.floor(Math.random()*5);
 return quotes[random];
 }
-
+//function checks for year or citation and replaces the quote on screen
 function printQuote(){
   var q = getRandomQuote();
   console.log(q.quote);
